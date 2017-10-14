c1f01fce33e7eab4cd7af5d8b1ffef3a4effb709
OnLiU <jhs_onliu@naver.com>
2017-10-14 22:28:54 +0900

    separate indexing, extractIndex, codeSpace, operationMap

---
+++ src/indexing.js
@@ -0,0 +1,14 @@
+const extractIndex = require('./extractIndex')
+const { cho, jung, jong } = require('./table').character
+
+module.exports.cho = extractIndex(cho, (syllableCode) => {
+  return (syllableCode / 588) | 0
+})
+
+module.exports.jung = extractIndex(jung, (syllableCode) => {
+  return (((syllableCode / 28) | 0) % 21) | 0
+})
+
+module.exports.jong = extractIndex(jong, (syllableCode) => {
+  return (syllableCode % 28) | 0
+})
